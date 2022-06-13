26172146b22f98e76b37b1210c2fed29167e9af4
TongThanaphon <tongasura001@gmail.com>
2022-06-13 16:11:35 +0700

    update

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const displayResultEle = document.getElementById("display-result");
 const latEle = document.getElementById("lat");
 const lngEle = document.getElementById("lng");
 
-const googleMapApiKey = "AIzaSyB2H0SO85XGT6TKqs-Vv255ASQFCXJgH3k";
+const googleMapApiKey = env.GOOGLE_MAP_API_KEY;
 const options = {
   fields: ["formatted_address", "geometry", "name"],
   strictBounds: false,
